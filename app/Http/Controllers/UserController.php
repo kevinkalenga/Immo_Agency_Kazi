30b4c7db1cd7cdaec02f8e9bd6c61a07b9d7c064
kevin <kevinkalenga10@gmail.com>
2025-12-30 10:42:35 +0100

    User Profile Design Part2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 
@@ -9,5 +12,12 @@ class UserController extends Controller
     public function index()
     {
         return view('frontend.home.index');
+    }
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $userData = User::find($id);
+
+        return view('frontend.dashboard.edit_profile', compact('userData'));
     }
 }
--- resources/views/frontend/dashboard/edit_profile.blade.php
+++ resources/views/frontend/dashboard/edit_profile.blade.php
@@ -1,11 +1,10 @@
-
+    
 @extends('frontend.frontend_dashboard')       
         
 @section('main')
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-<!--Page Title-->
+    
+    <!--Page Title-->
         <section class="page-title centred" style="background-image: url({{asset('frontend/assets/images/background/page-title-5.jpg')}});">
             <div class="auto-container">
                 <div class="content-box clearfix">
@@ -17,22 +16,26 @@
                 </div>
             </div>
         </section>
-<!--End Page Title-->
+        <!--End Page Title-->
 
- <!-- sidebar-page-container -->
+
+        
+
+        <!-- sidebar-page-container -->
         <section class="sidebar-page-container blog-details sec-pad-2">
             <div class="auto-container">
                 <div class="row clearfix">
                     
 
 
-
+             
         @php 
           
           $id = Auth::user()->id;
           $userData = App\Models\User::find($id);
 
         @endphp 
+
 
 
 
@@ -47,7 +50,7 @@
                         <div class="post">
                             <figure class="post-thumb"><a href="blog-details.html">
        <img id="showImage" src="{{(!empty($userData->photo)) ? url('uploads/user_images/'.$userData->photo) : url('uploads/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80"></a></figure>
-        <h5><a href="blog-details.html">{{$userData->name}}</a></h5>
+        <h5><a href="blog-details.html">{{$userData->name}} </a></h5>
          <p>{{$userData->email}} </p>
                         </div> 
                     </div>
@@ -55,9 +58,9 @@
        
         <div class="sidebar-widget category-widget">
             <div class="widget-title">
-               
+                
             </div>
-             @include('frontend.dashboard.dashboard_sidebar')
+             @include('frontend.dashboard.dashboard_sidebar')     
           </div> 
                          
                         </div>
@@ -72,58 +75,51 @@
                                 <div class="inner-box">
                                     
                                     <div class="lower-content">
-                                        <h3>Including Animation In Your Design System.</h3>
+                                        
                                       
                                       
           
+ <form action="signin.html" method="post" class="default-form">
+        <div class="form-group">
+            <label >Name</label>
+            <input name="email" type="text" class="form-control" value="{{$userData->name}}">
+        </div>
+        <div class="form-group">
+            <label>Username</label>
+            <input name="username" type="text" class="form-control" value="{{$userData->username}}">
+        </div>
+        <div class="form-group">
+           <label class="form-label">Email address</label>
+			<input name="email" type="email" class="form-control" value="{{$userData->email}}">
+        </div>
+        
+        
+        <div class="mb-3">
+			<label class="form-label">Phone</label>
+			<input name="phone" type="text" class="form-control" value="{{$userData->phone}}">
+		</div>
+		<div class="mb-3">
+			<label class="form-label">Address</label>
+			<input name="address" type="text" class="form-control" value="{{$userData->address}}">
+		</div>
+								  
+        <div class="mb-3">
+			<label class="form-label">Photo</label>
+			<input type="file" name="photo" class="form-control" value="{{$userData->photo}}" id="image" />
+		</div>
+        <div class="mb-3">
+			<label class="form-label"></label>
+			<img id="showImage" src="{{(!empty($userData->photo)) ? url('uploads/user_images/'.$userData->photo) : url('uploads/no_image.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
+		</div>
+        
+        
+        
 
 
-<div class="row">
-<div class="col-lg-4">
-    <div class="card-body" style="background-color: #1baf65;">
-    <h1 class="card-title" style="color: white; font-weight: bold;">0</h1>
-    <h5 class="card-text"style="color: white;"> Approved properties</h5>
-    
-  </div>
-</div>
-
-<div class="col-md-4">
-    <div class="card-body" style="background-color: #ffc107;">
-    <h1 class="card-title" style="color: white; font-weight: bold; ">0</h1>
-    <h5 class="card-text"style="color: white;"> Pending approve properties</h5>
-    
-  </div>
-</div>
-
-
-<div class="col-md-4">
-    <div class="card-body" style="background-color: #002758;">
-    <h1 class="card-title" style="color: white; font-weight: bold;">0</h1>
-    <h5 class="card-text"style="color: white; "> Rejected properties</h5>
-    
-  </div>
-</div>
-    
-</div> 
-
-                                    </div>
-                                </div>
-                            </div>
-                             
-                            
-                        </div>
-
-
-    <div class="blog-details-content">
-                            <div class="news-block-one">
-                                <div class="inner-box">
-                                    
-                                    <div class="lower-content">
-                                        <h3>Activity Logs</h3>
-                                      <hr>
-                                      
-          
-
+        <div class="form-group message-btn">
+            <button type="submit" class="theme-btn btn-one">Save Changes </button>
+        </div>
+    </form>
 
  
 
@@ -134,22 +130,18 @@
                             
                         </div>
 
-
-
-
-
-
+ 
                     </div> 
 
 
                 </div>
             </div>
         </section>
-<!-- sidebar-page-container -->
+        <!-- sidebar-page-container -->
 
-<!-- subscribe-section -->
+        <!-- subscribe-section -->
         <section class="subscribe-section bg-color-3">
-            <div class="pattern-layer" style="background-image: url({{asset('frontend/assets/images/shape/shape-2.png')}});"></div>
+            <div class="pattern-layer" style="background-image: url(assets/images/shape/shape-2.png);"></div>
             <div class="auto-container">
                 <div class="row clearfix">
                     <div class="col-lg-6 col-md-6 col-sm-12 text-column">
@@ -171,9 +163,9 @@
                 </div>
             </div>
         </section>
-<!-- subscribe-section end -->
 
-    <script type="text/javascript">
+    
+        <script type="text/javascript">
                $(document).ready(function(){
 				$('#image').change(function(e){
 					var reader = new FileReader();
@@ -184,5 +176,5 @@
 				})
 			   })
 		</script>
-
+        <!-- subscribe-section end -->
 @endsection
