8e290e372db05c79a500970afb2ea7b1f6a3efa1
kevin <kevinkalenga10@gmail.com>
2026-01-01 02:00:27 +0100

    Property Type Crud Part2

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -25,6 +25,10 @@ License: For each use you must have a valid license purchased only from above li
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
   <!-- End fonts -->
 
+   	<!-- Plugin css for this page -->
+     <link rel="stylesheet" href="{{asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css')}}">
+	<!-- End plugin css for this page -->
+
 	<!-- core:css -->
 	<link rel="stylesheet" href="{{asset('backend/assets/vendors/core/core.css')}}">
 	<!-- endinject -->
@@ -81,9 +85,10 @@ License: For each use you must have a valid license purchased only from above li
 	<script src="{{asset('backend/assets/vendors/feather-icons/feather.min.js')}}"></script>
 	<script src="{{asset('backend/assets/js/template.js')}}"></script>
 	<!-- endinject -->
-
-	<!-- Custom js for this page -->
-  <script src="{{asset('backend/assets/js/dashboard-dark.js')}}"></script>
+  	
+  
+   <!-- Custom js for this page -->
+  <script src="{{asset('backend/assets/js/data-table.js')}}"></script>
 	<!-- End custom js for this page -->
 
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
@@ -110,6 +115,13 @@ License: For each use you must have a valid license purchased only from above li
  }
  @endif 
 </script>
+
+
+  <!-- Plugin js for this page -->
+  <script src="{{asset('backend/assets/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js')}}"></script>
+   <script src="{{asset('backend/assets/js/dashboard-dark.js')}}"></script>
+	<!-- End plugin js for this page -->
 
 </body>
 </html>    
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ require __DIR__.'/auth.php';
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
-     Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
+    Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
  });
 // Admin(auth to verify if is logged in)
 
