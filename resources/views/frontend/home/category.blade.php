02c9f6ddaf8b48eb2ae9d532b7043cda3190bb2a
kevin <kevinkalenga10@gmail.com>
2026-05-15 00:30:16 +0200

    Show Property Type In Frontend Part2

--- resources/views/frontend/home/category.blade.php
+++ resources/views/frontend/home/category.blade.php
@@ -11,13 +11,17 @@
                 <div class="inner-container wow slideInLeft animated" data-wow-delay="00ms" data-wow-duration="1500ms">
                     <ul class="category-list clearfix">
                       @foreach($pType as $item)
+                        @php   
+                          $property = App\Models\Property::where('ptype_id', $item->id)->get();
+
+                        @endphp
                         <li>
                             <div class="category-block-one">
                               
                                 <div class="inner-box">
                                     <div class="icon-box"><i class="{{$item->type_icon}}"></i></div>
                                     <h5><a href="property-details.html">{{$item->type_name}}</a></h5>
-                                    <span>52</span>
+                                    <span>{{count($property)}}</span>
                                 </div>
                             </div>
                         </li>
