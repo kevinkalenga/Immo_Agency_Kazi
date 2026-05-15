1bb1e63b6f7d557ffce447e7774134622d6d49c4
kevin <kevinkalenga10@gmail.com>
2026-05-16 01:16:47 +0200

    Setup Property Details Related Page

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -22,7 +22,9 @@ class IndexController extends Controller
         $property_amen = explode(',', $amenities);
         $multiImage = MultiImage::where('property_id', $id)->get();
         $facility = Facility::where('property_id', $id)->get();
+        $type_id = $property->ptype_id;
+        $relatedProperty = Property::where('ptype_id', $type_id)->where('id', '!=', $id)->orderBy('id', 'DESC')->limit(3)->get();
         
-        return view('frontend.property.property_details', compact('property', 'multiImage', 'property_amen', 'facility'));
+        return view('frontend.property.property_details', compact('property', 'multiImage', 'property_amen', 'facility', 'relatedProperty'));
     }
 }
