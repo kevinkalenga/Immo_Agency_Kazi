722247532a03642f5cb7ff1dab31b10101d89b2d
kevin <kevinkalenga10@gmail.com>
2026-05-17 14:51:37 +0200

    Send Message To Agent Part3

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -12,6 +12,8 @@ use App\Models\PackagePlan;
 use App\Models\User;
 use App\Models\MultiImage;
 use App\Models\PropertyMessage;
+use Carbon\Carbon;
+use Auth;
 
 
 class IndexController extends Controller
@@ -28,8 +30,43 @@ class IndexController extends Controller
         
         return view('frontend.property.property_details', compact('property', 'multiImage', 'property_amen', 'facility', 'relatedProperty'));
     }
-    public function PropertyMessage($Request, $request)
+    public function PropertyMessage(Request $request)
     {
-        
+        $pId = $request->property_id;
+        $aId = $request->agent_id;
+
+        if (Auth::check()) {
+            
+        PropertyMessage::insert([
+
+            'user_id' => Auth::user()->id,
+            'agent_id' => $aId,
+            'property_id' => $pId,
+            'msg_name' => $request->msg_name,
+            'msg_email' => $request->msg_email,
+            'msg_phone' => $request->msg_phone,
+            'message' => $request->message,
+            'created_at' => Carbon::now(), 
+
+        ]);
+
+        $notification = array(
+            'message' => 'Send Message Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+
+
+        }else{
+
+            $notification = array(
+            'message' => 'Plz Login Your Account First',
+            'alert-type' => 'error'
+        );
+
+        return redirect()->back()->with($notification);
+        }
     }
 }
