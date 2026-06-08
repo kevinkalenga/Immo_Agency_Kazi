c8dd54ce630a5955a296430036f2f03789ea3065
kevin <kevinkalenga10@gmail.com>
2026-06-08 02:44:00 +0200

    Comment Function Backend With Reply Part3

--- resources/views/frontend/blog/blog_details.blade.php
+++ resources/views/frontend/blog/blog_details.blade.php
@@ -2,7 +2,9 @@
 @section('main') 
 
 
-
+@php
+ $comment = App\Models\Comment::where('post_id',$blog->id)->where('parent_id',null)->limit(5)->get();
+@endphp
 
 
 
@@ -61,58 +63,67 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="comments-area">
+                              @php
+                                $comment = App\Models\Comment::where('post_id',$blog->id)->where('parent_id',null)->limit(5)->get();
+                             @endphp
+                            
+                            
+                             <div class="comments-area">
                                 <div class="group-title">
-                                    <h4>3 Comments</h4>
+                                    <h4>Comments</h4>
                                 </div>
                                 <div class="comment-box">
+                                  @foreach($comment as $com)
                                     <div class="comment">
                                         <figure class="thumb-box">
-                                            <img src="assets/images/news/comment-1.jpg" alt="">
+                                            <img src="{{ (!empty($com->user->photo)) ? url('uploads/user_images/'.$com->user->photo) : url('uploads/no_image.jpg') }}" alt="">
                                         </figure>
                                         <div class="comment-inner">
                                             <div class="comment-info clearfix">
-                                                <h5>Rebeka Dawson</h5>
-                                                <span>April 10, 2020</span>
+                                                <h5>{{ $com->user->name }}</h5>
+                                                <span>{{ $com->created_at->format('M d Y') }}</span>
                                             </div>
                                             <div class="text">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nos trud exerc.</p>
+                                                <p>{{ $com->subject  }}</p>
+                                                <p>{{ $com->message  }}</p>
                                                 <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="comment replay-comment">
-                                        <figure class="thumb-box">
-                                            <img src="assets/images/news/comment-2.jpg" alt="">
-                                        </figure>
-                                        <div class="comment-inner">
-                                            <div class="comment-info clearfix">
-                                                <h5>Elizabeth Winstead</h5>
-                                                <span>April 10, 2020</span>
-                                            </div>
-                                            <div class="text">
-                                                <p>Lorem ipsum dolor sit amet, consectur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nos</p>
-                                                <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="comment">
-                                        <figure class="thumb-box">
-                                            <img src="assets/images/news/comment-3.jpg" alt="">
-                                        </figure>
-                                        <div class="comment-inner">
-                                            <div class="comment-info clearfix">
-                                                <h5>Benedict Cumbatch</h5>
-                                                <span>April 10, 2020</span>
-                                            </div>
-                                            <div class="text">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nos trud exerc.</p>
-                                                <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
+                                  
+                                     @php
+                                         $reply = App\Models\Comment::where('parent_id',$com->id)->get();
+                                     @endphp
+                                     @foreach($reply as $rep)
+                                        <div class="comment replay-comment">
+                                            <figure class="thumb-box">
+                                                <img src="{{ url('uploads/admin_images/202605302123t1.jpg') }}" alt="">
+                                            </figure>
+                                            <div class="comment-inner">
+                                                <div class="comment-info clearfix">
+                                                    <h5>{{ $rep->subject }}</h5>
+                                                    <span>{{ $rep->created_at->format('M d Y') }}</span>
+                                                </div>
+                                                <div class="text">
+                                                    <p>{{ $rep->message }}</p>
+                                                    <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    @endforeach
+                                  
+                                  @endforeach 
                                 </div>
                             </div>
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
                             <div class="comments-form-area">
                                 <div class="group-title">
                                     <h4>Leave a Comment</h4>
