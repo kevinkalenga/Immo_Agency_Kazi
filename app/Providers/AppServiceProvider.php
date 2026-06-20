a66b0445ef8b5187cf9c535bc476408eda7972f8
kevin <kevinkalenga10@gmail.com>
2026-06-20 02:19:10 +0200

    Dynamic Email Configuration Part3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Models\SmtpSetting;
+use Config;
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        try {
+            if (class_exists('Illuminate\Support\Facades\Schema') &&
+                \Illuminate\Support\Facades\Schema::hasTable('smtp_settings')) {
+
+                $smtpsetting = \App\Models\SmtpSetting::first();
+
+                if ($smtpsetting) {
+                    $data = [
+                        'driver' => $smtpsetting->mailer,
+                        'host' => $smtpsetting->host,
+                        'port' => $smtpsetting->port,
+                        'username' => $smtpsetting->username,
+                        'password' => $smtpsetting->password,
+                        'encryption' => $smtpsetting->encryption,
+                        'from' => [
+                            'address' => $smtpsetting->from_address,
+                            'name' => 'RealEstate'
+                        ]
+                    ];
+
+                    Config::set('mail', $data);
+                }
+            }
+        } catch (\Throwable $e) {
+        // ne jamais casser l'app
+        }
     }
 }
