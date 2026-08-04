36167ae647a3c64b4fca543fd8b13b2589a3e60b
kevin <kevinkalenga10@gmail.com>
2026-08-05 01:47:48 +0200

    Site Setting Option Part3

--- resources/views/frontend/home/footer.blade.php
+++ resources/views/frontend/home/footer.blade.php
@@ -1,3 +1,11 @@
+        
+   @php  
+    
+     $setting = App\Models\SiteSetting::find(1);
+     $blog = App\Models\BlogPost::latest()->limit(2)->get();
+
+    @endphp    
+        
         <footer class="main-footer">
             <div class="footer-top bg-color-2">
                 <div class="auto-container">
@@ -20,12 +28,12 @@
                                 </div>
                                 <div class="widget-content">
                                     <ul class="links-list class">
-                                        <li><a href="index.html">About Us</a></li>
-                                        <li><a href="index.html">Listing</a></li>
-                                        <li><a href="index.html">How It Works</a></li>
-                                        <li><a href="index.html">Our Services</a></li>
-                                        <li><a href="index.html">Our Blog</a></li>
-                                        <li><a href="index.html">Contact Us</a></li>
+                                        <li><a href="javascript:;">About Us</a></li>
+                                        <li><a href="javascript:;">Listing</a></li>
+                                        <li><a href="javascript:;">How It Works</a></li>
+                                        <li><a href="javascript:;">Our Services</a></li>
+                                        <li><a href="{{route('blog.list')}}">Our Blog</a></li>
+                                        <li><a href="javascript:;">Contact Us</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -36,16 +44,13 @@
                                     <h3>Top News</h3>
                                 </div>
                                 <div class="post-inner">
-                                    <div class="post">
-                                        <figure class="post-thumb"><a href="blog-details.html"><img src="{{asset('frontend/assets/images/resource/footer-post-1.jpg')}}" alt=""></a></figure>
-                                        <h5><a href="blog-details.html">The Added Value Social Worker</a></h5>
-                                        <p>Mar 25, 2020</p>
-                                    </div>
-                                    <div class="post">
-                                        <figure class="post-thumb"><a href="blog-details.html"><img src="{{asset('frontend/assets/images/resource/footer-post-2.jpg')}}" alt=""></a></figure>
-                                        <h5><a href="blog-details.html">Ways to Increase Trust</a></h5>
-                                        <p>Mar 24, 2020</p>
-                                    </div>
+                                    @foreach($blog as $item)
+                                      <div class="post">
+                                        <figure class="post-thumb"><a href="{{url('blog/details/'.$item->post_slug)}}"><img src="{{asset($item->post_image)}}" alt=""></a></figure>
+                                        <h5><a href="{{url('blog/details/'.$item->post_slug)}}">{{$item->post_title}}</a></h5>
+                                        <p>{{$item->created_at->format('M d Y')}}</p>
+                                      </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
@@ -56,9 +61,9 @@
                                 </div>
                                 <div class="widget-content">
                                     <ul class="info-list clearfix">
-                                        <li><i class="fas fa-map-marker-alt"></i>Flat 20, Reynolds Neck, North Helenaville, FV77 8WS</li>
-                                        <li><i class="fas fa-microphone"></i><a href="tel:23055873407">+2(305) 587-3407</a></li>
-                                        <li><i class="fas fa-envelope"></i><a href="mailto:info@example.com">info@example.com</a></li>
+                                        <li><i class="fas fa-map-marker-alt"></i>{{$setting->company_address}}</li>
+                                        <li><i class="fas fa-microphone"></i><a href="tel:23055873407">+{{$setting->company_phone}}</a></li>
+                                        <li><i class="fas fa-envelope"></i><a href="mailto:info@example.com">{{$setting->email}}</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -69,13 +74,13 @@
             <div class="footer-bottom">
                 <div class="auto-container">
                     <div class="inner-box clearfix">
-                        <figure class="footer-logo"><a href="index.html"><img src="{{asset('frontend/assets/images/footer-logo.png')}}" alt=""></a></figure>
+                        <figure class="footer-logo"><a href="javascript:;"><img src="{{asset('frontend/assets/images/footer-logo.png')}}" alt=""></a></figure>
                         <div class="copyright pull-left">
-                            <p><a href="index.html">Realshed</a> &copy; 2021 All Right Reserved</p>
+                            <p><a href="javascript:;">{{$setting->copyright}}</p>
                         </div>
                         <ul class="footer-nav pull-right clearfix">
-                            <li><a href="index.html">Terms of Service</a></li>
-                            <li><a href="index.html">Privacy Policy</a></li>
+                            <li><a href="javascript:;">Terms of Service</a></li>
+                            <li><a href="javascript:;">Privacy Policy</a></li>
                         </ul>
                     </div>
                 </div>
